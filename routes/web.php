bb2db58168430db8dc6bf8518bb29bf8204f003f
Home Labs <duck@localhost.localdomain>
2025-12-03 05:20:26 +0700

    feat: Add AdminController.php with update route to admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome-laravel');
+});
+
+Route::prefix('admin')->group(function (){
+    Route::get('/', [AdminController::class, 'index']);
 });
