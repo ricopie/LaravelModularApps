9c31880960ca61e22ef36840914f8d84a0414ea2
Home Labs <duck@localhost.localdomain>
2025-12-18 17:19:24 +0700

    chore(routes): temporarily redirect root to admin layout for dev mode

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome-laravel');
-});
-
-Route::prefix('admin')->group(function (){
-    Route::get('/', [AdminController::class, 'index']);
+    return view('layouts/admin');
 });
