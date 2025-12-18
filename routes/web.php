062bc35db3894bd5e5a7ff7ff4b1a359ef0ba93c
ricopie <myiico.sorapps@hotmail.com>
2025-12-18 21:17:03 +0700

    chore: add route to admin

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('layouts/admin');
+    return view('welcome-laravel');
+});
+
+// Route to Admin Panel
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index']);
 });
